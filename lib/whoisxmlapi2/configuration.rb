5453fc116e4f7d0f14d8972e335dc870ce3f6c5c
Maksim Litvinov <1058433+rxx@users.noreply.github.com>
2020-07-27 21:05:56 +0300

    Fix bug with mocking, add ability to configure with params

--- lib/whoisxmlapi2/configuration.rb
+++ lib/whoisxmlapi2/configuration.rb
@@ -5,20 +5,35 @@ module WhoisXMLAPI2
 
     DEFAULT_SERVICE_ENDPOINT = "https://whoisxmlapi.com/whoisserver/WhoisService?".freeze
 
-    def initialize
-      @url = DEFAULT_SERVICE_ENDPOINT
+    def initialize(params = {})
+      apply_configuration(params)
     end
 
-    def self.set?
-      WhoisXMLAPI2.configuration.url && \
-      WhoisXMLAPI2.configuration.api_key
+    class << self
+      def set?
+        WhoisXMLAPI2.configuration.url && \
+        WhoisXMLAPI2.configuration.api_key || \
+        WhoisXMLAPI2.configuration.mock_out_for_testing
+      end
+
+      def set_v1?
+        WhoisXMLAPI2.configuration.url && \
+        WhoisXMLAPI2.configuration.username && \
+        WhoisXMLAPI2.configuration.api_key && \
+        WhoisXMLAPI2.configuration.secret || \
+        WhoisXMLAPI2.configuration.mock_out_for_testing
+      end
     end
 
-    def self.set_v1?
-      WhoisXMLAPI2.configuration.url && \
-      WhoisXMLAPI2.configuration.username && \
-      WhoisXMLAPI2.configuration.api_key && \
-      WhoisXMLAPI2.configuration.secret
+    def apply_configuration(params)
+      params[:url] ||= DEFAULT_SERVICE_ENDPOINT
+
+      @username = params[:username]
+      @api_key = params[:api_key]
+      @secret = params[:secret]
+      @url = params[:url]
+      @browser_key = params[:browser_key]
+      @mock_out_for_testing = params[:mock_out_for_testing]
     end
   end
 end
